a2c8d50f6750879cf2807d95244c0967e19110c9
Hu <2671165557@qq.com>
2021-08-21 16:07:26 +0800

    初始版本

--- msj_v0/settings.py
+++ msj_v0/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'msj_v0.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': {
+        'ENGINE': 'django.db.backends.mysql',    # 数据库引擎
+        'NAME': 'xxqcmb', # 数据库名称
+        'HOST': '123.60.65.156', # 数据库地址，本机 ip 地址 127.0.0.1 
+        'PORT': 3306, # 端口 
+        'USER': 'root',  # 数据库用户名
+        'PASSWORD': 'XXQcmb666', # 数据库密码
     }
 }
 
